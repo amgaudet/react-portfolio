6cf5de0f8d73cbd8d129ea11c24edb04d75f3433
Shreekz <alanmguadet@gmail.com>
2021-08-15 20:23:53 -0400

    added tech stack icons

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import self from '../assets/self.png';
+import { Link } from 'react-router-dom';
 import '../style.css';
 
 const AboutMe = () => {
@@ -10,8 +11,8 @@ const AboutMe = () => {
         <img className="m-3" src={self} alt='avatar'></img>
         <p className="m-3 para">I am 33 years old Full Stack Web Developer currently in Clemmons, NC. I graduated
           from the UNC-Charlotte Full Stack Web Development Program in July 2021. Currently, I am refining my strengths
-          in my own projects and expanding my skills set. A current list of my skill set can be found here.
-          Previously, I worked as a personal trainer at commercial gyms across
+          in my own projects and expanding my skills set. A current list of my skill set can be found &nbsp;
+          <Link to='/react-portfolio'>here</Link>. Previously, I worked as a personal trainer at commercial gyms across
           North Carolina. I come from a competitive hands on environment and plan to implement those practices
           into my new found career path. In my spare time, I can be found playing a few select games, such as
           Starcraft II or outside playing ultimate frisbee. I'm also a big fan of the original Mega Man series and
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,43 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../style.css';
 
+import boot from '../assets/bootstrap.png';
+import css from '../assets/css1.png';
+import git from '../assets/git.png';
+import html from '../assets/htmllogo.png';
+import jquery from '../assets/jquery.png';
+import js from '../assets/jslogo.png';
+import mongo from '../assets/mongodb.png';
+import mysql from '../assets/mysql.png';
+import node from '../assets/nodejs.png';
+import react from '../assets/react.png'
+
+
 const Home = () => {
 
   return (
     <div className="landing text-center">
-      <h1>Shreekz coding Solutions</h1>
-      <h2>by Alan Gaudet</h2>
-      <p className="linkP"><Link to="/about">More about me</Link> &ensp;|&ensp;
-        <Link to="/portfolio">See what I've done</Link></p>
-      <p className="linkP"><Link to="/contactme">Contact Me</Link></p>
+      <div>
+        <h1>Shreekz coding Solutions</h1>
+        <h2>by Alan Gaudet</h2>
+        <p className="linkP"><Link to="/about">More about me</Link> &ensp;|&ensp;
+          <Link to="/portfolio">See what I've done</Link></p>
+        <p className="linkP pb-3 mb-5"><Link to="/contactme">Contact Me</Link></p>
+      </div>
+
+      <div className="skills py-5">
+        <h2 classname="mb-5">Powered By</h2>
+        <img className="icons" src={html} alt="htmlTag"></img>
+        <img className="icons" src={css} alt="cssTag"></img>
+        <img className="icons" src={js} alt="JavaScriptTag"></img>
+        <img className="icons" src={jquery} alt="JQueryTag"></img>
+        <img className="icons" src={react} alt="reactjsTag"></img>
+        <img className="icons" src={boot} alt="bootstrapTag"></img>
+        <img className="icons" src={mysql} alt="mySQLtag"></img>
+        <img className="icons" src={mongo} alt="MongoDBTag"></img>
+        <img className="icons" src={node} alt="NodejsTag"></img>
+        <img className="icons" src={git} alt="gitTag"></img>
+      </div>
 
     </div>
   )
